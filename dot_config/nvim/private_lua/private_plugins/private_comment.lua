775cf4bfc2b326b86bb505a743d719ab3b40ac72
jenkin <jenkinpan028@gmail.com>
2024-06-24 22:05:47 +0800

    Update .config/nvim/lua/plugins/comment.lua Update .config/nvim/lua/plugins/devicon.lua

--- dot_config/nvim/private_lua/private_plugins/private_comment.lua
+++ dot_config/nvim/private_lua/private_plugins/private_comment.lua
@@ -1,7 +1,1 @@
-return {
-	{
-		"folke/todo-comments.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {},
-	},
-}
+
--- dot_config/nvim/private_lua/private_plugins/private_devicon.lua
+++ dot_config/nvim/private_lua/private_plugins/private_devicon.lua
@@ -1,10 +1,1 @@
-return {
-	"rachartier/tiny-devicons-auto-colors.nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons"
-	},
-	event = "VeryLazy",
-	config = function()
-		require('tiny-devicons-auto-colors').setup()
-	end
-}
+
