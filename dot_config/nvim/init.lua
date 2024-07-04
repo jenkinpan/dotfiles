63f2c10dc05c9e4e2a39e94cccd6a844a48fe24a
jenkin <jenkinpan028@gmail.com>
2024-07-04 10:52:18 +0800

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -18,21 +18,6 @@ require("core.neovide")
 
 -- import plugins, and the file location is `nvim/lua/plugins`
 require("lazy").setup({ import = "plugins" }, {
-	pkg = {
-		enabled = true,
-		cache = vim.fn.stdpath("state") .. "/lazy/pkg-cache.lua",
-		-- the first package source that is found for a plugin will be used.
-		sources = {
-			"lazy",
-			"rockspec", -- will only be used when rocks.enabled is true
-			"packspec",
-		},
-	},
-	rocks = {
-		enabled = true,
-		root = vim.fn.stdpath("data") .. "/lazy-rocks",
-		server = "https://nvim-neorocks.github.io/rocks-binaries/",
-	},
 	checker = {
 		enabled = true,
 	},
