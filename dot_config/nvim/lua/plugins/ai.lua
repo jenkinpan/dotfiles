abef03715c31ee8a36efd49fa8d9125c5b78cba3
jenkin <jenkinpan028@gmail.com>
2024-07-06 21:02:40 +0800

    Update .config/nvim/lua/plugins/ai.lua

--- dot_config/nvim/lua/plugins/ai.lua
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -25,6 +25,21 @@ return {
 		lazy = true,
 		event = "InsertEnter",
 		branch = "canary",
+		opts = {
+			window = {
+				layout = "float", -- 'vertical', 'horizontal', 'float', 'replace'
+				width = 0.5, -- fractional width of parent, or absolute width in columns when > 1
+				height = 0.5, -- fractional height of parent, or absolute height in rows when > 1
+				-- Options below only apply to floating windows
+				relative = "editor", -- 'editor', 'win', 'cursor', 'mouse'
+				border = "rounded", -- 'none', single', 'double', 'rounded', 'solid', 'shadow'
+				row = nil, -- row position of the window, default is centered
+				col = nil, -- column position of the window, default is centered
+				title = "Copilot Chat", -- title of chat window
+				footer = nil, -- footer of chat window
+				zindex = 1, -- determines if window is on top or below other floating windows
+			},
+		},
 		keys = {
 			{ "<leader>ao", "<cmd>CopilotChatOptimize<cr>", desc = "Optimize Selected Code", mode = "v" },
 			{ "<leader>af", "<cmd>CopilotChatFix<cr>", desc = "Fix (selected)Code", mode = { "v", "n" } },
@@ -52,21 +67,6 @@ return {
 					require("CopilotChat").toggle()
 				end,
 				desc = "Toggle CopilotChat",
-			},
-		},
-		opts = {
-			window = {
-				layout = "float", -- 'vertical', 'horizontal', 'float', 'replace'
-				width = 0.5, -- fractional width of parent, or absolute width in columns when > 1
-				height = 0.5, -- fractional height of parent, or absolute height in rows when > 1
-				-- Options below only apply to floating windows
-				relative = "editor", -- 'editor', 'win', 'cursor', 'mouse'
-				border = "rounded", -- 'none', single', 'double', 'rounded', 'solid', 'shadow'
-				row = nil, -- row position of the window, default is centered
-				col = nil, -- column position of the window, default is centered
-				title = "Copilot Chat", -- title of chat window
-				footer = nil, -- footer of chat window
-				zindex = 1, -- determines if window is on top or below other floating windows
 			},
 		},
 	},
