e5ee11362215b56fd1a059a2327c8aebb3c588d8
jenkin <jenkinpan028@gmail.com>
2024-08-21 08:50:38 +0800

    Update .config/nvim/lua/plugins/dashboard.lua Update .config/nvim/spell/en.utf-8.add Update .config/nvim/spell/en.utf-8.add.spl

--- dot_config/nvim/lua/plugins/dashboard.lua
+++ dot_config/nvim/lua/plugins/dashboard.lua
@@ -17,8 +17,6 @@ return {
 			local opts = {
 				theme = "doom",
 				hide = {
-					-- this is taken care of by lualine
-					-- enabling this messes up the actual laststatus setting after loading a file
 					statusline = false,
 				},
 				config = {
