757535f01045c81c4288035dc94a2071f58b15be
jenkin <jenkinpan0628@gmail.com>
2025-09-07 19:31:46 +0800

    remove some unused configs

--- dot_config/nvim/lua/plugins/trouble.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"folke/trouble.nvim",
-	event = "BufRead",
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-	},
-}
