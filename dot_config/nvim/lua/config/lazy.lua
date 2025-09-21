02451703fca5212f2a7881dc82f0914e60b6e9b8
jenkin <jenkinpan0628@gmail.com>
2025-09-21 19:27:46 +0800

    Update .config/nvim/lua/config/lazy.lua

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -28,7 +28,7 @@ require("lazy").setup({
   install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
     enabled = true, -- check for plugin updates periodically
-    notify = false, -- notify on update
+    notify = true, -- notify on update
   },
   performance = {
     rtp = {
