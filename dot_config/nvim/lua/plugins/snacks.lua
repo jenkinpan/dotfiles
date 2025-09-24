e6b8f174aa0a8ea552f1d016384574a914311e2f
jenkin <jenkinpan0628@gmail.com>
2025-09-24 22:03:17 +0800

    Update .config/nvim/lua/plugins/snacks.lua

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -1,21 +1,19 @@
 return {
-  "folke/snacks.nvim",
-  opts = {
-    -- enable snakes.image
-    image = { enable = true },
-    terminal = {
-      enable = true,
-      win = {
-        -- make the terminal float
-        position = "float",
-        backdrop = 60,
-        height = 0.7,
-        width = 0.7,
-        border = "rounded",
-        title = "Terminal",
-        title_pos = "center",
-        zindex = 50,
-      },
-    },
-  },
+	"folke/snacks.nvim",
+	opts = {
+		-- enable snakes.image
+		image = { enable = true },
+		terminal = {
+			enable = true,
+			win = {
+				-- make the terminal float
+				position = "float",
+				backdrop = 60,
+				height = 0.7,
+				width = 0.7,
+				border = "rounded",
+				zindex = 50,
+			},
+		},
+	},
 }
