d8e9eda6aa02178b9b3d1b52183c83db78153d6a
jenkin <jenkinpan0628@gmail.com>
2025-10-05 20:28:21 +0800

    Update .config/nvim/lua/config/keymaps.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,7 +2,7 @@
 -- Add any additional keymaps here
 local map = vim.keymap.set
 -- set navagation between vim and tmux panes
-map("n", "<c-h>", "<cmd> TmuxNavigateLeft<cr>", { desc = "Navigate to left pane", remap = true })
-map("n", "<c-j>", "<cmd> TmuxNavigateDown<cr>", { desc = "Navigate to lower pane", remap = true })
-map("n", "<c-k>", "<cmd> TmuxNavigateUp<cr>", { desc = "Navigate to upper pane", remap = true })
-map("n", "<c-l>", "<cmd> TmuxNavigateRight<cr>", { desc = "Navigate to right pane", remap = true })
+map("n", "<c-h>", "<cmd>TmuxNavigateLeft<cr>", { desc = "Navigate to left pane", remap = true })
+map("n", "<c-j>", "<cmd>TmuxNavigateDown<cr>", { desc = "Navigate to lower pane", remap = true })
+map("n", "<c-k>", "<cmd>TmuxNavigateUp<cr>", { desc = "Navigate to upper pane", remap = true })
+map("n", "<c-l>", "<cmd>TmuxNavigateRight<cr>", { desc = "Navigate to right pane", remap = true })
