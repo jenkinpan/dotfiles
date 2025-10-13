45df38d2f76a2cab0210e1744c20855dfd48d738
jenkin <jenkinpan0628@gmail.com>
2025-10-13 19:06:47 +0800

    Update .config/fish/config.fish Update .config/fish/fish_variables Update .config/ghostty/config Update .config/starship.toml

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -11,10 +11,12 @@ fish_add_path /opt/homebrew/sbin $PATH
 
 # Cursor styles
 set -gx fish_vi_force_cursor 1
-set -gx fish_cursor_default block blink
+set -gx fish_cursor_default block
 set -gx fish_cursor_insert line blink
 set -gx fish_cursor_visual block
 set -gx fish_cursor_replace_one underscore
+
+set -gx fish_vi_key_bindings
 
 # editor
 set -gx EDITOR (which nvim)
