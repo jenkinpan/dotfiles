7aa110dcb092e5643da4006a31f8a3d1a63bb241
jenkin <jenkinpan0628@gmail.com>
2025-10-14 15:48:43 +0800

    Update .config/ghostty/config Update .config/nvim/lua/plugins/scheme.lua

--- dot_config/nvim/lua/plugins/scheme.lua
+++ dot_config/nvim/lua/plugins/scheme.lua
@@ -7,7 +7,7 @@ return {
       sidebars = "transparent",
       floats = "transparent",
     },
-    -- set lualine transparent
+    -- set lualine and snake.explore transparent
     on_colors = function(colors)
       colors.bg_statusline = colors.none
     end,
