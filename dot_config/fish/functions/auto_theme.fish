9908b0190e79472397bc3c0fc0368ec59514d0dc
jenkin <jenkinpan0628@gmail.com>
2025-10-22 22:40:40 +0800

    Update .config/fish/functions/auto_theme.fish

--- dot_config/fish/functions/auto_theme.fish
+++ dot_config/fish/functions/auto_theme.fish
@@ -1,16 +1,18 @@
 function auto_theme
-    # 检测系统主题
+    # detect system theme
     set system_theme (defaults read -g AppleInterfaceStyle 2>/dev/null)
-    
-    if test "$system_theme" = "Dark"
-        # 系统是暗色主题，使用暗色 Fish 主题
+
+    if test "$system_theme" = Dark
+        # when system in dark mode, use dark Fish theme and fzf theme
         if not test "$fish_theme" = "Catppuccin Mocha"
             fish_config theme choose "Catppuccin Mocha"
+            source ~/.config/fish/themes/fzf/catppuccin-fzf-mocha.fish
         end
     else
-        # 系统是亮色主题，使用亮色 Fish 主题
+        # when system in light mode, use light Fish theme and fzf theme
         if not test "$fish_theme" = "Catppuccin Latte"
             fish_config theme choose "Catppuccin Latte"
+            source ~/.config/fish/themes/fzf/catppuccin-fzf-latte.fish
         end
     end
 end
