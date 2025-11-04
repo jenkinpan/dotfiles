a57afe4791695dfcb981fd425c34feab7374b386
jenkin <jenkinpan0628@gmail.com>
2025-11-04 22:57:41 +0800

    Add .config/fish/completions/docker.fish Add .config/fish/completions/kubectl.fish Add .config/fish/completions/orbctl.fish Add .config/fish/conf.d/git.fish Add .config/fish/.DS_Store Add .config/fish/functions/__git.branch_has_wip.fish Add .config/fish/functions/__git.current_branch.fish Add .config/fish/functions/__git.default_branch.fish Add .config/fish/functions/__git.destroy.fish Add .config/fish/functions/__git.init.fish Add .config/fish/functions/brew.fish Add .config/fish/functions/fish_greeting.fish Add .config/fish/functions/gbage.fish Add .config/fish/functions/gbda.fish Add .config/fish/functions/gdv.fish Add .config/fish/functions/gignored.fish Add .config/fish/functions/glp.fish Add .config/fish/functions/grename.fish Add .config/fish/functions/grt.fish Add .config/fish/functions/gtest.fish Add .config/fish/functions/gtl.fish Add .config/fish/functions/gunwip.fish Add .config/fish/functions/gwip.fish Add .config/fish/fish_variables Add .config/fish/themes/.DS_Store

---
+++ dot_config/fish/functions/fish_greeting.fish
@@ -0,0 +1,9 @@
+function fish_greeting -d "Display system information after shell initialization"
+    # 确保只在交互式 shell 中运行（例如，排除脚本执行）
+    if status --is-interactive
+        # 运行 fastfetch，使用您配置中的确切参数
+        if type -q fastfetch
+            fastfetch --logo none --config examples/13
+        end
+    end
+end
